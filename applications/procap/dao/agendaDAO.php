505f200b3885f0f2b2078e3772c817d1e10f3fdb
Anselmo S Ribeiro <6975.usa@gmail.com>
2015-06-06 16:55:52 -0500

    1 e 4 - Lancamentos com inicio e termino entram automatico na agenda.

--- applications/procap/dao/agendaDAO.php
+++ applications/procap/dao/agendaDAO.php
@@ -16,62 +16,57 @@
  * @copyright	2010 Anselmo S Ribeiro
  * @licence	LGPL
  */
+class procap_dao_agendaDAO extends classes_dao_AbstractDAO {
 
-class procap_dao_agendaDAO extends classes_dao_AbstractDAO
-{
+    public $table = 'procap_andamento';
 
-   public $table = 'procap_andamento';
+    function __construct() {
+        parent::__construct();
+    }
 
-   function __construct( ){
-      parent::__construct();
-   }
+    public function setConnString() {
+        return procap_config_DatabaseConfiguration::getConn('procap');
+    }
 
+    public function setMdb2Conn() {
+        return procap_config_DatabaseConfiguration::getConn('procap');
+    }
 
-   public function setConnString(){
-      return procap_config_DatabaseConfiguration::getConn('procap');
-   }
+    public function getConnInfo() {
+        return procap_config_DatabaseConfiguration::getConnInfo('procap');
+    }
 
-   public function setMdb2Conn(){
-      return procap_config_DatabaseConfiguration::getConn('procap');
-   }
+    public function getListNames() {
 
-   public function getConnInfo(){
-      return procap_config_DatabaseConfiguration::getConnInfo('procap');
-   }
+        //pr($_GET['dias']) ;
 
+        $where = null;
+        if (!empty($_GET['dias'])) {
+            $dias = $_GET['dias'];
+            $where = " and (  ";
+            $where .= " ( a.termino_data >= '{$dias[0]}' and a.termino_data <= '{$dias[1]}' )  ";
+            if (!isset($_GET['semana_da_agenda']) || ( isset($_GET['semana_da_agenda']) && $_GET['semana_da_agenda'] == 'stual' )) {
+                $where .= " or ( a.termino_data < '{$dias[0]}' and conclusao_data is null ) ";
+            }
+            $where .= "   )   ";
+        } else {
+            $where = " and ( a.termino_data =  '{$_GET['dia']}' )  ";
+        }
+        $where.=" and ( a.inicio_data is not null and a.termino_data is not null and a.conclusao_data is null )";
 
-   public function getListNames(){
+        $user = classes_Singleton::getInstance('classes_controller_user');
 
-      //pr($_GET['dias']) ;
+        $tipo = $this->getPessoaTipo($user->id);
+        $user = classes_Singleton::getInstance('classes_controller_user');
 
-      $where = null ;
-      if(!empty($_GET['dias'])){
-         $dias = $_GET['dias'];
-         $where = " and (  ";
-         $where .= " ( a.termino_data >= '{$dias[0]}' and a.termino_data <= '{$dias[1]}' )  ";
-         if( !isset($_GET['semana_da_agenda']) ||  ( isset($_GET['semana_da_agenda']) && $_GET['semana_da_agenda'] == 'stual'  )  ){
-            $where .= " or ( a.termino_data < '{$dias[0]}' and conclusao_data is null ) ";
-         }
-         $where .= "   )   ";
-      }
-      else{
-         $where = " and ( a.termino_data =  '{$_GET['dia']}' )  ";
-      }
+        if ($user->inGroup('Administrador')) {
+            $w = $where;
+        } else {
+            $w = "  and pes.user_id = '{$user->id}' $where ";
+        }
 
-      $user = classes_Singleton::getInstance('classes_controller_user');
-
-      $tipo = $this->getPessoaTipo($user->id);
-      $user = classes_Singleton::getInstance('classes_controller_user');
-
-      //if($user->inGroup('Administrador')){
-         $w = $where ;
-      //}
-      //else{
-         //TODO $w = "  and pes.user_id = '{$user->id}' $where " ;
-      //}
-
-      $sql =   array(
-      'agendaList' => "
+        $sql = array(
+            'agendaList' => "
                SELECT
                   a.id
                   ,a.id as andamento_id
@@ -85,7 +80,7 @@ class procap_dao_agendaDAO extends classes_dao_AbstractDAO
                   ,concat(date_format(a.termino_data,'%d/%m/%Y'),' ', case weekday(a.termino_data) when 6 then 'Dom' when 0 then 'Seg' when 1 then 'Ter'when 2 then 'Qua' when 3 then 'Qui' when 4 then 'Sex' when 5 then 'Sab' End ) as termino_data
                   ,concat(date_format(a.conclusao_data,'%d/%m/%Y'),' ', case weekday(a.conclusao_data) when 6 then 'Dom' when 0 then 'Seg' when 1 then 'Ter'when 2 then 'Qua' when 3 then 'Qui' when 4 then 'Sex' when 5 then 'Sab' End ) as conclusao_data
                   ,a.descricao
-                  ,case a.agenda when 1 then 'Sim' when 0 then 'Não' End as agenda
+                  ,case a.agenda when 1 then 'Sim' when 0 then 'Nï¿½o' End as agenda
                   ,p.numero as processo_nr
                   ,pes.nome as nome
                   , concat( ifnull( ( select concat( ifnull(nome,'') , ifnull(nome_fantasia,'') ) as nome from procap_pessoa where procap_pessoa.id = cliente1_id ) ,'') , ifnull( ( select concat( ifnull(nome,'') , ifnull(nome_fantasia,'') ) from procap_pessoa where procap_pessoa.id = cliente2_id ) ,'') ) as cliente
@@ -98,48 +93,31 @@ class procap_dao_agendaDAO extends classes_dao_AbstractDAO
                where a.agenda = 1 $w
                order by a.termino_data asc , a.id asc
                "
-               );
-      return $sql;
+        );
+        return $sql;
+    }
 
-   }
+    function getNextIdVal($pk) {
+        return $this->conn->genId($this->table . $pk);
+    }
 
+    function getagendas() {
+        $sql = "select distinct id as codigo  , descricao from procap_agenda order by descricao  ";
+        $res = $this->execute($sql);
+        return $res->getAssoc();
+    }
 
-   function getNextIdVal($pk){
-      return $this->conn->genId($this->table.$pk) ;
-   }
-
-
-
-
-   function getagendas(){
-      $sql = "select distinct id as codigo  , descricao from procap_agenda order by descricao  ";
-      $res = $this->execute($sql);
-      return $res->getAssoc() ;
-
-   }
-
-
-
-
-   function getPessoaTipo($pessoaId){
-      $sql = " select tipo from procap_pessoa where user_id = ? ";
-      $res = $this->execute($sql,array($pessoaId));
-      if($res->numRows() < 1 ){
-         return null;
-      }
-      elseif($res->numRows() > 1){
-         throw new Exception('Há mais de um advogado com o mesmo Nome de Login');
-      }
-      else {
-         return $res->fields('tipo') ;
-      }
-   }
-
-
-
-
-
-
+    function getPessoaTipo($pessoaId) {
+        $sql = " select tipo from procap_pessoa where user_id = ? ";
+        $res = $this->execute($sql, array($pessoaId));
+        if ($res->numRows() < 1) {
+            return null;
+        } elseif ($res->numRows() > 1) {
+            throw new Exception('Hï¿½ mais de um advogado com o mesmo Nome de Login');
+        } else {
+            return $res->fields('tipo');
+        }
+    }
 
 }
 
